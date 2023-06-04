e1833fd90eb9dfa7c041dac7e11618134055c07e
Franco459 <francopistonecarp11@gmail.com>
2023-06-04 10:53:24 -0300

    Agregar archivos de proyecto.

---
+++ Tiendaapi/Controllers/Modelo/ProductosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tiendaapi.Controllers.Modelo
+{
+    public class ProductosModel
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; } //tipo money en SQL
+    }
+}
---
+++ Tiendaapi/Controllers/ProductosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiendaapi.Controllers.Modelo;
+
+namespace Tiendaapi.Controllers
+{
+    [ApiController]
+    [Route("api/productos")]
+    public class ProductosController
+    {
+        [HttpGet]
+        public async Task <ActionResult<List<ProductosModel>>> Get()
+        {
+
+        }
+        public async ProdcutosModel GetProdcutos()
+        {
+
+        }
+    }
+}
