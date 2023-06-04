58e3801607ae57fa1aca7d515f182f3316a36057
Franco459 <francopistonecarp11@gmail.com>
2023-06-04 11:06:09 -0300

    Conexion con DB

---
+++ Tiendaapi/Conexion/ConexionDB.cs
@@ -0,0 +1,18 @@
+﻿namespace Tiendaapi.Conexion
+{
+    public class ConexionDB
+    {
+        private string connectionString = string.Empty;
+        public ConexionDB()
+        {
+            var constr = new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            connectionString = constr.GetSection("ConnectionStrings: conexionDB").Value;
+
+        }
+        public string conexionSQL()
+        {
+            return connectionString;
+        }
+    }
+}
